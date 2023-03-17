50ccd63b5333014fdec8675522c75ca7088efa4e
jesuscumpli <jesuscumpli@uma.es>
2023-03-17 20:13:12 +0100

    retraining and tunning

--- services/retraining/mainRetrainingService.py
+++ services/retraining/mainRetrainingService.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     while True:
         try:
             logging.info("** RETRAINING TASK: Comenzando Reentrenamiento....")
-            adsModel.retrain_model(random=True, size_split=size_split, epochs=epochs, tunning=False, model_by_best_epoch=True)
+            tunning = random.uniform(0, 1) > 0.8
+            adsModel.retrain_model(random=True, size_split=size_split, epochs=epochs, tunning=tunning, model_by_best_epoch=True)
             logging.info("** RETRAINING TASK: FIN REENTRENAMIENTO")
             size_split += 100
             epochs = random.randint(10, 100) #inclucive

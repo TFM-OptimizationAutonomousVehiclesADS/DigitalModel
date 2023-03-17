fa356951d15e92a9735bf70c98c6414bc6fb9e07
jesuscumpli <jesuscumpli@uma.es>
2023-03-17 20:12:10 +0100

    retraining and tunning

--- services/retraining/mainRetrainingService.py
+++ services/retraining/mainRetrainingService.py
@@ -8,12 +8,12 @@ SLEEP_TIME = 60*2
 if __name__ == "__main__":
     logging.info("** RETRAINING TASK: Iniciando Modelo de Detección de Anomalías....")
     adsModel = ADSModel()
-    size_split = 2000
+    size_split = 5000
     epochs = 10
     while True:
         try:
             logging.info("** RETRAINING TASK: Comenzando Reentrenamiento....")
-            adsModel.retrain_model(random=True, size_split=size_split, epochs=epochs, tunning=True, model_by_best_epoch=True)
+            adsModel.retrain_model(random=True, size_split=size_split, epochs=epochs, tunning=False, model_by_best_epoch=True)
             logging.info("** RETRAINING TASK: FIN REENTRENAMIENTO")
             size_split += 100
             epochs = random.randint(10, 100) #inclucive

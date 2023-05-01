b523908be73f087d3c1d1811496579c402160693
Jesus Cumplido <jesus.cumplido@ravenloop.io>
2023-05-02 00:26:42 +0200

    model digital v6

--- start.sh
+++ start.sh
@@ -12,15 +12,26 @@ python3 /app/ADS/initTraining.py > /proc/1/fd/1 2>&1
 sleep 5
 
 # Iniciar los servicios de Python
-uvicorn services.api.mainApiService:app --host 0.0.0.0 --port 8001 &
-sleep 5
+#uvicorn services.api.mainApiService:app --host 0.0.0.0 --port 8001 &
+#sleep 5
+#
+#python3 /app/services/detection/mainDetectionService.py > /proc/1/fd/1 2>&1 &
+#python3 /app/services/simulator/mainRandomSamplesGeneratorService.py > /proc/1/fd/1 2>&1 &
+#if [ -n "${IS_REAL_SYSTEM+x}" ] && [ "$IS_REAL_SYSTEM" = "1" ]; then
+#    echo "IS_REAL_SYSTEM is set to 1. Skipping mainRetrainingService.py."
+#else
+#    python3 /app/services/retraining/mainRetrainingService.py > /proc/1/fd/1 2>&1 &
+#fi
 
-python3 /app/services/detection/mainDetectionService.py > /proc/1/fd/1 2>&1 &
-python3 /app/services/simulator/mainRandomSamplesGeneratorService.py > /proc/1/fd/1 2>&1 &
+systemctl start api.service
+sleep 5
+systemctl start ads.service
+systemctl start simulator.service
+sleep 5
 if [ -n "${IS_REAL_SYSTEM+x}" ] && [ "$IS_REAL_SYSTEM" = "1" ]; then
     echo "IS_REAL_SYSTEM is set to 1. Skipping mainRetrainingService.py."
 else
-    python3 /app/services/retraining/mainRetrainingService.py > /proc/1/fd/1 2>&1 &
+    systemctl start retraining.service
 fi
 
 tail -f /dev/null
